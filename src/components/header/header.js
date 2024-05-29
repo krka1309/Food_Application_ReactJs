13f85aea7e5c700a9378866b76c991a164afa9ec
krka0922 <krishna.kadam@netcracker.com>
2024-05-29 23:25:26 +0530

    navbar routing added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./header.css";
 import restaurant from "../../assets/images/resturant.jpeg";
+import Cart from "../../pages/cart/cart";
 import Navbar from "./navbar";
 const Header = () => {
   return (
     <div className="mainDiv">
       <img src={restaurant} alt="" className="restaurantImg" />
-      {/* <Navbar /> */}
+      <Navbar />
+      <Cart />
     </div>
   );
 };
