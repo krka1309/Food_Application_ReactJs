08370644dae8a86c62a5781f850c75cf6c2d06ec
krka0922 <krishna.kadam@netcracker.com>
2024-05-29 23:08:51 +0530

    restaurant image and css added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./header.css";
+import restaurant from "../../assets/images/resturant.jpeg";
+import Navbar from "./navbar";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="mainDiv">
+      <img src={restaurant} alt="" className="restaurantImg" />
+      {/* <Navbar /> */}
+    </div>
+  );
 };
 
 export default Header;
