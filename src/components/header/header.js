6dfb48a0837e84f4d7bc7d9c8f3bc4aa8b671653
krka0922 <krishna.kadam@netcracker.com>
2024-06-01 17:51:19 +0530

    navigation bar appear on each page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./header.css";
 import restaurant from "../../assets/images/resturant.jpeg";
-import Cart from "../../pages/cart/cart";
+import Card from "../../pages/card/card";
 import Navbar from "./navbar";
 const Header = () => {
   return (
     <div className="mainDiv">
       <img src={restaurant} alt="" className="restaurantImg" />
       <Navbar />
-      <Cart />
+      <Card />
     </div>
   );
 };
--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-import "./cart.css";
-import { BiShoppingBag } from "react-icons/bi";
-import { AiOutlineClose } from "react-icons/ai";
-const Cart = () => {
-  const [cartOpen, setCartOpen] = useState(false);
-  const handleCart = () => {
-    setCartOpen(true);
-  };
-  console.log(cartOpen);
-  return (
-    <div className="cartMainDiv">
-      <BiShoppingBag
-        className="bagIcon"
-        onClick={() => setCartOpen(!cartOpen)}
-      />
+import React from "react";
 
-      <div className={cartOpen ? "cartOpen" : "cartClose"}>
-        <p>Shopping list</p>
-        <AiOutlineClose
-          onClick={() => setCartOpen(false)}
-          className="outlineIcon"
-        />
-      </div>
-    </div>
-  );
+const Cart = () => {
+  return <div>Cart</div>;
 };
 
 export default Cart;
