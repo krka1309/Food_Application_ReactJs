90062cc61b44c966a05af9dc15f888c6b21ce446
krka0922 <krishna.kadam@netcracker.com>
2024-06-01 17:24:58 +0530

    shopping drawer added

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,7 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./cart.css";
+import { BiShoppingBag } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 const Cart = () => {
-  return <div>Cart</div>;
+  const [cartOpen, setCartOpen] = useState(false);
+  const handleCart = () => {
+    setCartOpen(true);
+  };
+  console.log(cartOpen);
+  return (
+    <div className="cartMainDiv">
+      <BiShoppingBag
+        className="bagIcon"
+        onClick={() => setCartOpen(!cartOpen)}
+      />
+
+      <div className={cartOpen ? "cartOpen" : "cartClose"}>
+        <p>Shopping list</p>
+        <AiOutlineClose
+          onClick={() => setCartOpen(false)}
+          className="outlineIcon"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
