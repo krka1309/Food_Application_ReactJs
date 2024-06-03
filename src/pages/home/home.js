8be45748f7639aa35e6a313f0c808b52f1499dd8
krka0922 <krishna.kadam@netcracker.com>
2024-06-03 23:36:32 +0530

    delivery comp added

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,12 +4,14 @@ import boy from "../../assets/images/boyA.jpeg";
 import "./home.css";
 import Hero from "../hero/hero";
 import FoodCard from "../card/foodCard";
+import Delivery from "../hero/delivery";
 
 const Home = () => {
   return (
     <div className="homeMainDiv">
       <Hero />
       <FoodCard />
+      <Delivery />
     </div>
   );
 };
