7ec74e3fe8c21da4aa6573f8a1b830bbf0b4e632
krka0922 <krishna.kadam@netcracker.com>
2024-06-06 11:38:22 +0530

    products container added

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header/header";
 import boy from "../../assets/images/boyA.jpeg";
 import "./home.css";
 import Hero from "../hero/hero";
 import FoodCard from "../card/foodCard";
+import burger from "../../assets/imagesAll/hamburger.png";
+import pizza from "../../assets/imagesAll/pizza.png";
+import bread from "../../assets/imagesAll/bread.png";
+import products from "../../assets/fake-data/products";
 import Delivery from "../hero/delivery";
+import Products from "../../components/productsCard/products";
 
 const Home = () => {
+  const [allProducts, setAllProducts] = useState(products);
   return (
     <div className="homeMainDiv">
       <Hero />
       <FoodCard />
       <Delivery />
+      <div className="homeMainHeadline">
+        <h2>Popular Foods</h2>
+        <div className="childDiv1">
+          <button className="titleAll">All</button>
+          <button className="titleImages">
+            <img src={burger} />
+            Burger
+          </button>
+          <button className="titleImages">
+            <img src={pizza} />
+            Pizza
+          </button>
+          <button className="titleImages">
+            <img src={bread} />
+            Bread
+          </button>
+        </div>
+      </div>
+      <div className="productMainContainer">
+        <div className="productsContainer">
+          {allProducts.map((item) => {
+            return <Products item={item} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
