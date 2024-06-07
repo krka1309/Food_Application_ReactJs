6f3efeb13c36987ab1103d83444ec0023f5db29b
krka0922 <krishna.kadam@netcracker.com>
2024-06-07 18:11:37 +0530

    cart and contact component done

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./cart.css";
+import Contact from "../contact/contact";
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <div className="cartMainDiv1">Your Cart</div>
+      <div className="cartMainDiv11">No Items Added here</div>
+      <Contact />
+    </div>
+  );
 };
 
 export default Cart;
